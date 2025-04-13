1ab1aff99f5c97dfb994f4ef019c48b638d20e91
Patrick LEBER <patrick.leber@a1.digital>
2025-04-13 23:05:57 +0200

    fix(cluster-config): fix wrong server insecure config

--- setup.sh
+++ setup.sh
@@ -16,3 +16,14 @@ helm upgrade --install argocd argo/argo-cd \
 kustomize build ./argocd/bootstrap/overlays/interview-gitops-sks | kubectl apply -f -
 
 kustomize build ./argocd/components/appprojects | kubectl apply -f -
+
+echo "Waiting for Argo CD admin password to become available..."
+for i in {1..30}; do
+  if kubectl -n argocd get secret argocd-initial-admin-secret &>/dev/null; then
+    ARGOCD_ADMIN_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+    printf "Admin password: \033[31m%s\033[0m\n" "$ARGOCD_ADMIN_PASSWORD"
+    break
+  fi
+  echo "Waiting for secret... retry $i"
+  sleep 5
+done
